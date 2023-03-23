b833348887cbbd34a0995349a3c130db12b0cc52
Job Muturi <muirimuturi17@gmail.com>
2023-03-23 12:42:25 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    buildToDo(e.target.new_todo.value)
+  }
+  )
 });
+function buildToDo(todo){
+  let p = document.createElement('p');
+  p.textContent = todo  
+  let btn= document.createElement('button');
+  btn.addEventListener('click', handleDelete)
+  p.appendChild(btn)  
+  btn.textContent = ' x '
+  document.querySelector('#tasks').appendChild(p)
+
+}
+function handleDelete (e) {
+  e.target.parentNode.remove()
+}
